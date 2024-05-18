507d2b64461826085f6c8c1ecf88d8b92a719e82
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:28:28 +0500

    File of q111

---
+++ q111.ts
@@ -0,0 +1,20 @@
+//Use an if-else-if chain to categorize a person's age group (child, teenager, adult).
+
+
+// This function categorizes a person's age group
+function categorizeAge(age: number) : string {
+if (age < 13){
+    return "child";
+} 
+else if (age <= 19){
+    return "teenager";
+}
+else { 
+    return "adult";
+}
+}
+console.log(categorizeAge(12));         // Outputs: child
+console.log(categorizeAge(18));         // Outputs: teenager
+console.log(categorizeAge(25));         // outputs: adult
+
+
